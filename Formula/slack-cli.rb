2147d163f933e14b5356ac8e9f5e59a2e4ccde34
piekstra <piekstra@users.noreply.github.com>
2026-01-06 12:37:32 -0500

    Update slack-cli to v1.0.0 with SHA256 checksums

--- Formula/slack-cli.rb
+++ Formula/slack-cli.rb
@@ -6,23 +6,23 @@ class SlackCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/piekstra/slack-cli/releases/download/v#{version}/slack-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      url "https://github.com/piekstra/slack-cli/releases/download/v1.0.0/slack-cli_1.0.0_darwin_arm64.tar.gz"
+      sha256 "0d610770b1197cef22a54564554fd42eafebbefc141c957fed98a509e0f4da4c"
     end
     on_intel do
-      url "https://github.com/piekstra/slack-cli/releases/download/v#{version}/slack-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_AMD64"
+      url "https://github.com/piekstra/slack-cli/releases/download/v1.0.0/slack-cli_1.0.0_darwin_amd64.tar.gz"
+      sha256 "3bc16ad0653591fe045e16ee8529d09416e84f2cbac79d7753c688a79ee5978c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/piekstra/slack-cli/releases/download/v#{version}/slack-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      url "https://github.com/piekstra/slack-cli/releases/download/v1.0.0/slack-cli_1.0.0_linux_arm64.tar.gz"
+      sha256 "64acf4059c78a980d0a5ee7f8b34cafa2db4ef7b5a64826de4f1191ad526c187"
     end
     on_intel do
-      url "https://github.com/piekstra/slack-cli/releases/download/v#{version}/slack-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      url "https://github.com/piekstra/slack-cli/releases/download/v1.0.0/slack-cli_1.0.0_linux_amd64.tar.gz"
+      sha256 "29b46a8e0fc6957e09339c12fe2766071cb4aee166056e3af85487b1d90bb15d"
     end
   end
 
@@ -36,10 +36,6 @@ class SlackCli < Formula
         slack-cli config set-token <your-slack-api-token>
 
       Or set the SLACK_API_TOKEN environment variable.
-
-      For shell completions, run:
-        slack-cli completion bash > $(brew --prefix)/etc/bash_completion.d/slack-cli
-        slack-cli completion zsh > $(brew --prefix)/share/zsh/site-functions/_slack-cli
     EOS
   end
 
