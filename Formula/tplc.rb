f2ca08caa63035b1b795bfc1931056d0fef5647a
piekstra <piekstra@users.noreply.github.com>
2026-09-14 13:08:03 +0000

    Update tplc to v0.2.2

--- Formula/tplc.rb
+++ Formula/tplc.rb
@@ -2,21 +2,21 @@ class Tplc < Formula
   desc "TP-Link Kasa and Tapo smart-home devices from the terminal"
   homepage "https://github.com/piekstra/tplink-cloud-cli"
   license "GPL-3.0"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.1/tplc-aarch64-apple-darwin.tar.gz"
-      sha256 "dff163d7827a6d6ea09044526416cc308fa5fd7238f2ee3accf201eefad4fb0d"
+      url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.2/tplc-aarch64-apple-darwin.tar.gz"
+      sha256 "e21ba9d8ecd5fbd9608a06fa7c2f43f1d6c71ac132ebde2cc349724ce06223a3"
     else
-      url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.1/tplc-x86_64-apple-darwin.tar.gz"
-      sha256 "f75ad50826a37d33b41009e49382b4982e6504f38129f1f000985d9d330daf48"
+      url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.2/tplc-x86_64-apple-darwin.tar.gz"
+      sha256 "a79cb947475ffb05b8f56be289f7b20be154e48760e9cc343541d632ba08ec04"
     end
   end
 
   on_linux do
-    url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.1/tplc-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "df3015b869b49a702511a046d866c73ba3177bfec16f5ae5898decc627b407a9"
+    url "https://github.com/piekstra/tplink-cloud-cli/releases/download/v0.2.2/tplc-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "c421440290e3ea9f693f76620697f7339fbc0d1a3c83f6ee2df7c990415486e3"
   end
 
   def install
